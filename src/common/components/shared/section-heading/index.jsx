90b8fad2c1c0dbb5a50699a23d8db3deef020367
DeepuGagan <gagandeepu717@gmail.com>
2025-06-30 04:28:03 +0530

    Add section heading shared component

---
+++ src/common/components/shared/section-heading/index.jsx
@@ -0,0 +1,7 @@
+export default function SectionHeading({ children }) {
+  return (
+    <h2 className="mt-6 mb-12 text-center text-4xl font-semibold uppercase">
+      {children}
+    </h2>
+  );
+}
